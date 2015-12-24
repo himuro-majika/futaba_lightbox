d025d4abb5638c36f66db09f14f6890913ff1fdf
himuro-majika <himuro-majika@users.noreply.github.com>
2015-12-24 14:04:43 +0900

    Beautify source. Change soft tab to hard tab.

--- futaba_lightbox.user.js
+++ futaba_lightbox.user.js
@@ -19,146 +19,145 @@
 // ==/UserScript==
 this.$ = this.jQuery = jQuery.noConflict(true);
 
-(function ($) {
-    /*
-    設定
-    */
-    // 閉じるボタンを表示する
-    var USE_CLOSEBTN = false;
-    // 末尾から先頭にループさせる
-    var USE_LOOP = false;
-    // マウスホイールでのナビゲーションを使用する
-    var USE_MOUSEWHEEL = true;
-    // 該当レスにスクロールする
-    var USE_SCROLL = true;
-    // スクロールのなめらかさ
-    var SCROLL_DURATION = 100;
+(function($) {
+	/*
+	 * 設定
+	 */
+	// 閉じるボタンを表示する
+	var USE_CLOSEBTN = false;
+	// 末尾から先頭にループさせる
+	var USE_LOOP = false;
+	// マウスホイールでのナビゲーションを使用する
+	var USE_MOUSEWHEEL = true;
+	// 該当レスにスクロールする
+	var USE_SCROLL = true;
+	// スクロールのなめらかさ
+	var SCROLL_DURATION = 100;
 
-    init();
+	init();
 
-    function init(){
-        // var Start = new Date().getTime();//count parsing time
-        add_class_and_rel();
-        add_css();
-        setup_fancybox();
-        // console.log('Parsing : '+((new Date()).getTime()-Start) +'msec');//log parsing time
-    }
+	function init() {
+		// var Start = new Date().getTime();//count parsing time
+		add_class_and_rel();
+		add_css();
+		setup_fancybox();
+		// console.log('Parsing : '+((new Date()).getTime()-Start) +'msec');//log parsing time
+	}
 
-    function add_class_and_rel() {
-        var $sure_a = $("body > form > a > img").parent();
-        if($("#master").length) {   // ふたクロ
-            $sure_a = $("#master > a > img").parent();
-        }
-        if($("#threadsbox").length) {    // futaboard
-            $sure_a = $(".d7 > a > img").parent();
-        }
-        $sure_a.addClass("futaba_lightbox");
-        $sure_a.attr("rel", "futaba_lightbox_gallery");
+	function add_class_and_rel() {
+		var $sure_a = $("body > form > a > img").parent();
+		if ($("#master").length) { // ふたクロ
+			$sure_a = $("#master > a > img").parent();
+		}
+		if ($("#threadsbox").length) { // futaboard
+			$sure_a = $(".d7 > a > img").parent();
+		}
+		$sure_a.addClass("futaba_lightbox");
+		$sure_a.attr("rel", "futaba_lightbox_gallery");
 
-        add_class_res();
+		add_class_res();
 
-        setInterval(function(){
-            add_class_res();
-        }, 2000);
+		setInterval(function() {
+			add_class_res();
+		}, 2000);
 
-        function add_class_res() {
-            //  var Start = new Date().getTime();//count parsing time
-            var $res_a = $(".rtd > a > img").parent();
-            if($("#threadsbox").length) {    // futaboard
-                $res_a = $(".d6 > table img").parent();
-            }
-            $res_a.addClass('futaba_lightbox');
-            $res_a.attr("rel", "futaba_lightbox_gallery");
-            removeAkahukuThrop();
-            //  console.log('Parsing : '+((new Date()).getTime()-Start) +'msec');//log parsing time
-        }
-        // 赤福操作パネル対策
-        function removeAkahukuThrop() {
-            var $attb = $("#akahuku_throp_thumbnail_button[rel='futaba_lightbox_gallery']");
-            if($attb.length) {
-                $attb.removeClass("futaba_lightbox");
-                $attb.attr("rel", "");
-            }
-        }
-    }
+		function add_class_res() {
+			//  var Start = new Date().getTime();//count parsing time
+			var $res_a = $(".rtd > a > img").parent();
+			if ($("#threadsbox").length) { // futaboard
+				$res_a = $(".d6 > table img").parent();
+			}
+			$res_a.addClass('futaba_lightbox');
+			$res_a.attr("rel", "futaba_lightbox_gallery");
+			removeAkahukuThrop();
+			//  console.log('Parsing : '+((new Date()).getTime()-Start) +'msec');//log parsing time
+		}
+		// 赤福操作パネル対策
+		function removeAkahukuThrop() {
+			var $attb = $("#akahuku_throp_thumbnail_button[rel='futaba_lightbox_gallery']");
+			if ($attb.length) {
+				$attb.removeClass("futaba_lightbox");
+				$attb.attr("rel", "");
+			}
+		}
+	}
 
-    function add_css() {
-        var css = GM_getResourceText("fancyboxCSS");
-        GM_addStyle(css);
-        var sprite = GM_getResourceURL("fancyboxSprite");
-        GM_addStyle(
-            "#fancybox-loading, .fancybox-close, .fancybox-prev span, .fancybox-next span {" +
-            "  background-image: url(" + sprite + ");" +
-            "}" +
-            "#fancybox-loading div {" +
-            "  display: none;" +
-            "}" +
-            ".fancybox-nav {" +
-            "  background: transparent;" +
-            "  width: 45%;" +
-            "}" +
-            // ふたクロ書き込みウィンドウ対応
-            ".fancybox-opened {" +
-            "  z-index: 2000000013;" +
-            "}"
-        );
-    }
+	function add_css() {
+		var css = GM_getResourceText("fancyboxCSS");
+		GM_addStyle(css);
+		var sprite = GM_getResourceURL("fancyboxSprite");
+		GM_addStyle(
+			"#fancybox-loading, .fancybox-close, .fancybox-prev span, .fancybox-next span {" +
+			"  background-image: url(" + sprite + ");" +
+			"}" +
+			"#fancybox-loading div {" +
+			"  display: none;" +
+			"}" +
+			".fancybox-nav {" +
+			"  background: transparent;" +
+			"  width: 45%;" +
+			"}" +
+			// ふたクロ書き込みウィンドウ対応
+			".fancybox-opened {" +
+			"  z-index: 2000000013;" +
+			"}"
+		);
+	}
 
-    function setup_fancybox() {
-        $(".futaba_lightbox").fancybox({
-            margin : 15,                                         //画像外側のスペース
-            padding : 5,                                         //画像内側のスペース(白枠部)
-            openEffect : "none",                                 //開く時のエフェクト
-            closeEffect : "none",                                //閉じる時のエフェクト
-            prevEffect : "none",                                 //次移動時のエフェクト
-    		nextEffect : "none",                                 //前移動時のエフェクト
-            preload : "2",                                       //プリロードする画像の数
-            mouseWheel : USE_MOUSEWHEEL,
-    		closeBtn : USE_CLOSEBTN,                             //閉じるボタン
-            loop : USE_LOOP,                                     //末尾から先頭へのループ
-            helpers : {
-                overlay : {
-                    speedOut   : 100,                            //閉じる時の背景のフェード時間
-                    // showEarly : false,
-                    fixed : false,                               //固定表示(falseでスクロール可能)
-                    css : {
-                        // "background" : "rgba(0,0,0,0.85)"     //背景色
-                        background : "none"
-                    }
-                }
-            },
-            tpl : {
-                image    : '<a href="{href}" target="_blank"><img class="fancybox-image" src="{href}" alt="" /></a>',
-                error    : '<p class="fancybox-error">画像がないよ<br>すでに削除されてるかも</p>',
-                next     : '<a title="次" class="fancybox-nav fancybox-next"><span></span></a>',
-                prev     : '<a title="前" class="fancybox-nav fancybox-prev"><span></span></a>'
-            },
-            afterLoad: function(current, previous) {
-                // console.info( 'Current: ' + current.href );
-                // console.info( 'Previous: ' + (previous ? previous.href : '-') );
-                // if (previous) {
-                //     console.info( 'Navigating: ' + (current.index > previous.index ? 'right' : 'left') );
-                // }
-                if(USE_SCROLL){
-                    scrollToRes(current.href);
-                }
-            }
-        });
-    }
+	function setup_fancybox() {
+		$(".futaba_lightbox").fancybox({
+			margin: 15, //画像外側のスペース
+			padding: 5, //画像内側のスペース(白枠部)
+			openEffect: "none", //開く時のエフェクト
+			closeEffect: "none", //閉じる時のエフェクト
+			prevEffect: "none", //次移動時のエフェクト
+			nextEffect: "none", //前移動時のエフェクト
+			preload: "2", //プリロードする画像の数
+			mouseWheel: USE_MOUSEWHEEL,
+			closeBtn: USE_CLOSEBTN, //閉じるボタン
+			loop: USE_LOOP, //末尾から先頭へのループ
+			helpers: {
+				overlay: {
+					speedOut: 100, //閉じる時の背景のフェード時間
+					// showEarly : false,
+					fixed: false, //固定表示(falseでスクロール可能)
+					css: {
+						// "background" : "rgba(0,0,0,0.85)"     //背景色
+						background: "none"
+					}
+				}
+			},
+			tpl: {
+				image: '<a href="{href}" target="_blank"><img class="fancybox-image" src="{href}" alt="" /></a>',
+				error: '<p class="fancybox-error">画像がないよ<br>すでに削除されてるかも</p>',
+				next: '<a title="次" class="fancybox-nav fancybox-next"><span></span></a>',
+				prev: '<a title="前" class="fancybox-nav fancybox-prev"><span></span></a>'
+			},
+			afterLoad: function(current, previous) {
+				// console.info( 'Current: ' + current.href );
+				// console.info( 'Previous: ' + (previous ? previous.href : '-') );
+				// if (previous) {
+				//     console.info( 'Navigating: ' + (current.index > previous.index ? 'right' : 'left') );
+				// }
+				if (USE_SCROLL) {
+					scrollToRes(current.href);
+				}
+			}
+		});
+	}
 
-    function scrollToRes(currenthref) {
-        var $img_a = $(".futaba_lightbox[href='" + currenthref + "']").parent();
-        if($img_a.length){
-            var img_position = $img_a.offset().top;
-            $("html,body").animate({
-                scrollTop : img_position
-            },
-            {
-                duration: SCROLL_DURATION,
-                queue: false
-            });
-            // $("html,body").scrollTop(img_position);
-        }
-    }
+	function scrollToRes(currenthref) {
+		var $img_a = $(".futaba_lightbox[href='" + currenthref + "']").parent();
+		if ($img_a.length) {
+			var img_position = $img_a.offset().top;
+			$("html,body").animate({
+				scrollTop: img_position
+			}, {
+				duration: SCROLL_DURATION,
+				queue: false
+			});
+			// $("html,body").scrollTop(img_position);
+		}
+	}
 
 })(jQuery);
